9d80fdebe867593886686a9bf455318d59adacd5
Jean <vittory.dev@gmail.com>
2024-01-10 22:02:22 -0500

    Changing message and fixing port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express();
-
+const PORT = 3000
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.send('<h1>Welcome</h1>')
 })
 
-app.listen(3000, () => console.log('listening on port 3000'))
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
